ecb197714f68774f3d8d9db0ae5748ebfeaa0e7d
Gurmeetsingh7 <official.iamgurmeet@gmail.com>
2024-11-21 20:18:56 +0530

    first commit

---
+++ popup.js
@@ -0,0 +1,36 @@
+const btn = document.querySelector(".colorPickerBtn");
+const colorGrid = document.querySelector(".colorGrid");
+const colorValue = document.querySelector(".colorValue");
+btn.addEventListener("click", async () => {
+  let [tab] = await chrome.tabs.query({ active: true, currentWindow: true });
+
+  chrome.scripting.executeScript(
+    {
+      target: { tabId: tab.id },
+      function: pickColor,
+    },
+    async (injectionResults) => {
+      const [data] = injectionResults;
+      if (data.result) {
+        const color = data.result.sRGBHex;
+        colorGrid.style.backgroundColor = color;
+        colorValue.innerText = color;
+        try {
+          await navigator.clipboard.writeText(color);
+        } catch (err) {
+          console.log(err);
+        }
+      }
+    }
+  );
+});
+
+async function pickColor() {
+  try {
+    const eyeDropper = new EyeDropper();
+
+    return await eyeDropper.open();
+  } catch (err) {
+    console.log(err);
+  }
+}
